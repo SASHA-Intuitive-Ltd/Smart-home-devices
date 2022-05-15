4615eaa9cff17a1893bac0ea69e08264a8f77a17
Ruby Rudov <59509727+rubenrudov@users.noreply.github.com>
2022-05-15 11:48:00 +0300

    Changes: shutdown playsound

--- main.py
+++ main.py
@@ -4,21 +4,27 @@ __purpose__: 'Main function of voice recognition system for smart shower'
 """
 
 # Import modules
-import csv      # For handling csv command files
 import speech_recognition as sr     # For recognizing commands and write to file
 import os.path      # For removing files
 from pyModbusTCP.client import ModbusClient
 import json
-import playsound
+#import playsound
 from gtts import gTTS
 
+# Define speech recognizer
 RECOGNIZER = sr.Recognizer()
 
+# Define hebrew oriented commands dictionary
 json_file = open("commands.json", "r", encoding="utf8")
 REGISTERS = json.load(json_file)
 json_file.close()
 
-# Data
+# Define english oriented commands dictionary
+json_file1 = open("commands_eng.json", "r")
+REGISTERS_eng = json.load(json_file1)
+json_file1.close()
+
+# Define addresses and paths configs
 af = open("addresses.json", "r")
 addresses = json.load(af)
 af.close()
@@ -31,64 +37,101 @@ HMI_IP_ADDR = addresses["HMI_IP_ADDR"]
 PLC_IP_ADDR = addresses["PLC_IP_ADDR"]
 PLC_COM_PORT = addresses["PLC_COM_PORT"]
 
-
-HEB_TO_SAY = {
-    "עליון": "Upper",
-    "תחתון": "Lower",
-    "גב": "Back",
-    "ימני": "Right",
-    "שמאלי": "Left",
-    "ראש": "Hair",
-    "בידה": "Bidet",
-    "שטיפה": "Rinse",
-    "גוף": "Body",
-    "ייבוש": "Dry",
-    "עצור": "Stop",
-}
+# Modbus protocol client
+client = ModbusClient(host=PLC_IP_ADDR, port=PLC_COM_PORT, auto_open=True)
 
 
-def say(text, count: int):
+def say(text: str, active_lang: str, count: int):
     """
+    :param active_lang:
     :param count:
     :param text: str that contains the proffered voice output
     :return: None
     """
-    tts = gTTS(text=f"Activating: {HEB_TO_SAY[text]}", lang="en-US")
+    # Hebrew to English translate dict
+    HEB_TO_ENG = {
+        "עליון": "Upper",
+        "תחתון": "Lower",
+        "גב": "Back",
+        "ימני": "Right",
+        "שמאלי": "Left",
+        "ראש": "Hair",
+        "בידה": "Bidet",
+        "שטיפה": "Rinse",
+        "גוף": "Body",
+        "ייבוש": "Dry",
+        "עצור": "Stop",
+    }
+
+    # Text to speech settings
+    tts = gTTS(text=f"Activating: {HEB_TO_ENG[text]}", lang="en-US") if active_lang == 'he' else gTTS(text=f"Activating: {text}", lang="en-US")
+
+    # Audio file saving
     filename = f"voice{count}.mp3"
-    tts.save(filename)  # saves the audio file
-    playsound.playsound(filename)
+    tts.save(filename)
+
+    # Play the audio file
+    # playsound.playsound(filename)
+
+    # Clean file from memory
     os.remove(filename)
 
 
 # Write command to csv recipe file according to command type
-def write_commands(voice_cmd: str, count: int):
+def write_commands(voice_cmd: str, active_lang: str, count: int):
     """
-    :param count:
+    :param active_lang: current language (set in plc)
+    :param count: files count
     :param voice_cmd: The given voice command, translated to text
     :return: None
     """
-    client = ModbusClient(host=PLC_IP_ADDR, port=PLC_COM_PORT, auto_open=True)
 
     # TODO:
     # if 'צדדים' in voice_cmd:
     #     print("Single coil")
     #     client.write_single_coil(REGISTERS['left'], True)
     #     client.write_single_coil(REGISTERS['right'], True)
-    # print("Single coil")
 
-    client.write_single_coil(REGISTERS[voice_cmd], True)
-    say(voice_cmd, count)
+    # Write command by lang
+    if active_lang == 'he':
+        client.write_single_coil(REGISTERS[voice_cmd], True)
+
+    else:
+        client.write_single_coil(REGISTERS_eng[voice_cmd], True)
+
+    # Indicate user about command being activated
+    # say(voice_cmd, active_lang, count)
 
 
-# Main function
+def find_query(queries, reg_keys: dict) -> str:
+    """
+    Function for finding the correct key word by alternatives according to given keys
+    :param queries: all possible recognitions
+    :param reg_keys: language register keys
+    :return: str of query
+    """
+    for q in queries['alternative']:
+        if q['transcript'] in reg_keys:
+            return q['transcript'].replace("'", "")
+
+
 def main():
-    # TODO 1: Add multiple mics functionality
-    # TODO 5: Save active command (better ux - saying: "Finishing: wash hair..."
-
-    # Active until user says 'quit'
+    # Loop count
     count = 0
     while True:
+
+        # Read and print current language by M162 state
+        read_state = client.read_coils(2210)
+        print(read_state)
+
+        # Choose lang according to PLC button state
+        lang = "he" if read_state[0] is False else "en-US"
+        print(lang)
+
+        # When opening mic as source
         with sr.Microphone() as source:
+
+            # Indicate for debug
             print("Listening...")
 
             # Listeners setting
@@ -98,36 +141,41 @@ def main():
             try:
                 # Try recognize speech
                 print("Recognizing...")
+
+                # Recognize possible options, by audio, current lang and show all options
                 queries = RECOGNIZER.recognize_google(
                     audio,
-                    language="he",
+                    language=lang,
                     show_all=True,
                 )
 
+                # Print option for debugging
                 print(queries)
 
-                for q in queries['alternative']:
-                    if q['transcript'] in REGISTERS.keys():
-                        query = q['transcript'].lower()
-                        print(query)
+                # Choose the right option for command
+                query = find_query(queries, REGISTERS.keys()) if lang == 'he' else find_query(queries, REGISTERS_eng.keys())
 
                 # Generate commands file if command in the possible commands list
                 if query:
-                    write_commands(query, count)
+                    write_commands(query, lang, count)
 
                 else:
                     print("UNKNOWN COMMAND")
 
+            # Speech recognition error
             except sr.UnknownValueError:
                 # Throw exception if error
                 print("Could not understand audio")
 
+            # Print all exceptions
             except Exception as e:
                 print(e)
 
+            # Add to count each loop cycle
             finally:
                 count += 1
 
 
+# Run program
 if __name__ == '__main__':
     main()
